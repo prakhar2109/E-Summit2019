85b9e28214d03363ed8ffebfc39fa9a9f9e5a8f3
Pulkit Gupta <pulkonet@gmail.com>
2018-11-08 21:40:00 +0530

    fixed signup api

--- src/screens/register/register.jsx
+++ src/screens/register/register.jsx
@@ -1,216 +1,178 @@
-import React, { Component } from 'react';
-import './register.css';
-import Footer  from "../footer/footer";
-import { NavLink } from "react-router-dom"
+import React, { Component } from 'react'
+import './register.css'
+import Footer from '../footer/footer'
+import { NavLink } from 'react-router-dom'
 import Header from './../header/header'
-import axios from "axios"
-import Select from 'react-select';
+import axios from 'axios'
+import Select from 'react-select'
 
+const url = 'http://esummit.in/api/signup'
 
-const url = "http://esummit.in/api/signup";
-
-
-
- 
 const gender_option = [
-    {value: 0, label:"Male"},
-    {value: 1, label:"Female"},
-    {value: 2, label:"Others"},
-    {value: 3, label:"Prefer Not Say"},
+    { value: 0, label: 'Male' },
+    { value: 1, label: 'Female' },
+    { value: 2, label: 'Others' },
+    { value: 3, label: 'Prefer Not Say' }
 ]
 export default class ComingSoon extends Component {
- 
-    CollegeData = [];
-    state = {
-        name : "",
-        email: '',
-        contact : "",
-        password : '',
-        college : "",
-        state:"",
-        gender:"0",
-  
-
-      } 
-    componentDidMount() {
-        axios.get("http://esummit.in/api/college/list")
-            .then( res=> {
-
-               this.CollegeData = res.data.body;
+  CollegeData = []
+  state = {
+    name: '',
+    email: '',
+    contact: '',
+    password: '',
+    college: '',
+    state: '',
+    gender: '0',
+    collegeArray: []
+  }
+  componentDidMount () {
+    axios
+            .get('http://esummit.in/api/college/list')
+            .then(res => {
+              let CollegeData = res.data.body
+              CollegeData = CollegeData.map(item => ({ value: item.value, label: item.name }))
+              this.setState({ collegeArray: CollegeData })
             })
             .catch(function (response) {
-                alert(response);
-          });
-    }
-    
-    handleChange = (college) => {
-        this.setState({ college });
-         
-    }
-  
-    handleChange2 = (gender) => {
-        this.setState({ gender });
-        
-    }
+              alert(response)
+            })
+  }
+
+  handleChange = college => {
+    this.setState({ college })
+  }
+
+  handleChange2 = gender => {
+    this.setState({ gender })
+  }
   handleClick = e => {
-          e.preventDefault()
-          this.state.college = this.state.college["value"];
-          this.state.gender = this.state.gender["value"];
-          let data = this.state
-          this.setState({
-            name : "",
-            email: '',
-            contact : "",
-            password : '',
-            state:"",
-            gender:"0",
-           
-        })
+    e.preventDefault()
+    this.state.college = this.state.college['value']
+    this.state.gender = this.state.gender['value']
+    let data = this.state
+    this.setState({
+      name: '',
+      email: '',
+      contact: '',
+      password: '',
+      state: '',
+      gender: '0'
+    })
 
-
-        if(this.state.password.length < 8)
-        {
-            alert("Password length  must be greater than 8  ")
-        }
-
-        else{
-
-            axios({
-                method: 'post',
-                url: url,
-                data:data,
-                config: { headers: {'Content-Type': 'multipart/form-data' }}
-                })
+    if (this.state.password.length < 8) {
+      alert('Password length  must be greater than 8  ')
+    } else {
+      axios({
+        method: 'post',
+        url: url,
+        data: data,
+        config: { headers: { 'Content-Type': 'multipart/form-data' } }
+      })
                 .then(function (res) {
-                    window.location.href = '/login';
+                  window.location.href = '/login'
                 })
-                .catch(function   (response) {
-                        console.log(data);
-                        alert(response);
-                });
-            
-        }
+                .catch(function (response) {
+                  alert(response)
+                })
     }
+  }
 
+  render () {
+    const { college, gender, collegeArray } = this.state
 
+    return (
+      <div>
 
-    render() {
-        const {college, gender} = this.state ;
+        <Header />
 
-        return (
-            <>
+        <div className='register_main'>
 
-           
-            
+          <div className='register_text'>
 
-                <Header />
+            <span> CAMPUS AMBASSADOR</span>
 
-            <div className="register_main">
+            <span>
+                            The CAP( Campus Ambassador Programme) is a pinion initiative of E-Summit IIT Roorkee, 2018 organized by the Entrepreneurship Cell, IIT Roorkee and aims to amplify our purview to newer horizons. We aspire to increase our outreach to as many students as possible throughout the country and spread the spirit of entrepreneurship to similar extents. This year E-Summit IIT Roorkee brings with it loads and loads of colossal opportunities for the participating students and exciting perks for the Campus Ambassadors.
+                            {' '}
+            </span>
 
-                <div className="register_text">
+            <span>Have a look at the rulebook</span>
 
-                    <span> CAMPUS AMBASSADOR</span>
+            <center><button> RULEBOOK</button></center>
 
-                    <span>The CAP( Campus Ambassador Programme) is a pinion initiative of E-Summit IIT Roorkee, 2018 organized by the Entrepreneurship Cell, IIT Roorkee and aims to amplify our purview to newer horizons. We aspire to increase our outreach to as many students as possible throughout the country and spread the spirit of entrepreneurship to similar extents. This year E-Summit IIT Roorkee brings with it loads and loads of colossal opportunities for the participating students and exciting perks for the Campus Ambassadors. </span>
+          </div>
 
-                    <span>Have a look at the rulebook</span>
+          <div className='register_form'>
 
-                    <center><button> RULEBOOK</button></center>
+            <span className='register_login'>
+              {' '}<NavLink activeClassName='act' to='/login'>Sign In</NavLink>
+            </span>
+            <span className='register_register'>
+              {' '}<NavLink activeClassName='act' to='/register'>Sign Up</NavLink>
+            </span>
 
-                </div>
+            <form>
 
+              <label>NAME </label>
 
-                <div className="register_form">
-
-                               <span className = "register_login"> <NavLink activeClassName="act" to="/login">Sign In</NavLink></span>
-                               <span className = "register_register"> <NavLink activeClassName="act" to="/register">Sign Up</NavLink></span>
-
-                    <form >
-
-                        <label>NAME </label>
-
-                        <input 
-                            type="text"
-                            value={this.state.name}
-                            onChange={event => {
-                                    this.setState({
-                                        name: event.target.value
-                                    })
-                                }}
-                        ></input>
-
-                         
-
-                        <label>PHONE NO.</label>
-
-                        <input
-                         type="number"
-                         value={this.state.contact}
-
-                            onChange={event => {
-                                this.setState({
-                                    contact: event.target.value
-                                })
-                            }}
-
-
-                         ></input>
-
-                        <label>EMAIL-ID</label>
-
-                        <input 
-                        type="email"
-                        value={this.state.email}
-                        onChange={event => {
-                            this.setState({
-                                email: event.target.value
-                            })
-                        }}
-
-
-                        ></input>
-                        <label>PASSWORD </label>
-
-                        <input type="password"
-                        
-                        value={this.state.passowrd}
-
-                            onChange={event => {
-                                this.setState({
-                                    password: event.target.value
-                                })
-                            }}
-
-                        
-                        ></input>
-                        <label>COLLEGE </label>
-
-                     
-
-                        <Select
-                             
-                             value={college}
-                             onChange={this.handleChange}
-                             options = {this.CollegeData}
+              <input
+                type='text'
+                value={this.state.name}
+                onChange={event => {
+                  this.setState({
+                    name: event.target.value
+                  })
+                }}
                             />
 
-                          <label>GENDER </label>
+              <label>PHONE NO.</label>
 
-                          <Select
-                             
-                             value={gender}
-                             onChange={this.handleChange2}
-                             options={gender_option}
+              <input
+                type='number'
+                value={this.state.contact}
+                onChange={event => {
+                  this.setState({
+                    contact: event.target.value
+                  })
+                }}
                             />
 
-     
-                             
+              <label>EMAIL-ID</label>
 
-                        {/*
+              <input
+                type='email'
+                value={this.state.email}
+                onChange={event => {
+                  this.setState({
+                    email: event.target.value
+                  })
+                }}
+                            />
+              <label>PASSWORD </label>
+
+              <input
+                type='password'
+                value={this.state.passowrd}
+                onChange={event => {
+                  this.setState({
+                    password: event.target.value
+                  })
+                }}
+                            />
+              <label>COLLEGE </label>
+
+              <Select value={college} onChange={this.handleChange} options={collegeArray} />
+
+              <label>GENDER </label>
+
+              <Select value={gender} onChange={this.handleChange2} options={gender_option} />
+
+              {/*
                         <label>PROGRAMME </label>
 
-                        <input type="text" 
-                        
+                        <input type="text"
+
                         value={this.state.program}
 
                             onChange={event => {
@@ -219,11 +181,11 @@ export default class ComingSoon extends Component {
                                 })
                             }}
 
-                        ></input> 
+                        ></input>
                         <label>YEAR </label>
 
                         <input type="number"
-                        
+
                         value={this.state.year}
 
                             onChange={event => {
@@ -234,8 +196,8 @@ export default class ComingSoon extends Component {
                         ></input>
                         <label>CITY </label>
 
-                        <input type="text" 
-                        
+                        <input type="text"
+
                         value={this.state.city}
 
                             onChange={event => {
@@ -245,27 +207,24 @@ export default class ComingSoon extends Component {
                             }}
 
                         ></input>
-                            */} 
-                        <label>STATE </label>
+                            */}
+              <label>STATE </label>
 
-                        <input type="text" 
-                        
-                        value={this.state.state}
+              <input
+                type='text'
+                value={this.state.state}
+                onChange={event => {
+                  this.setState({
+                    state: event.target.value
+                  })
+                }}
+                            />
 
-                        onChange={event => {
-                            this.setState({
-                                state: event.target.value
-                            })
-                        }}
-
-                        
-                        ></input>
-
-                        {/*
+              {/*
                         <label>HOW DID YOU KNOW ABOUT CA </label>
 
-                        <input type="text" 
-                        
+                        <input type="text"
+
                         value={this.state.how}
 
                             onChange={event => {
@@ -274,29 +233,20 @@ export default class ComingSoon extends Component {
                                 })
                             }}
 
-                        ></input>*/}
+                        ></input> */}
 
+            </form>
 
+            <br />
 
-                    </form>
+            <button onClick={this.handleClick}> SIGN UP </button>
 
-                    <br />
+          </div>
+          <Footer />
 
+        </div>
 
-                    <button onClick = {this.handleClick} > SIGN UP </button>
-
-
-
-                </div>
-                <Footer/>
-
-
-
-            </div>
-
-            </>
-
-        );
-
-    }
+      </div>
+    )
+  }
 }
