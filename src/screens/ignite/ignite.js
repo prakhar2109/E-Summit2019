8c04ceb776500752caf39ac437919e5a3b14e22b
Pulkit Gupta <pulkonet@gmail.com>
2018-11-10 18:50:40 +0530

    changed startup ignite text

--- src/screens/ignite/ignite.js
+++ src/screens/ignite/ignite.js
@@ -1,226 +1,212 @@
-import React, { Component } from 'react'; 
+import React, { Component } from 'react'
 import jump from 'jump.js'
-import "./css/ignite.css";
+import './css/ignite.css'
 import Header from './../header/header'
-import scroll  from  "./static/Union.svg";
-import Footer  from "../footer/footer";
+import scroll from './static/Union.svg'
+import Footer from '../footer/footer'
 
-import jaipur from "../../utils/jaipur.jpg"
-import deh from "../../utils/dehradun.jpg"
-import chandi from "../../utils/chandigarh.jpg"
+import jaipur from '../../utils/jaipur.jpg'
+import deh from '../../utils/dehradun.jpg'
+import chandi from '../../utils/chandigarh.jpg'
 
-import guru from "../../utils/gurugram.jpg"
+import guru from '../../utils/gurugram.jpg'
 
-import case1 from  "../../utils/case1.png"
-import case2 from  "../../utils/Workshops.jpg"
+import case1 from '../../utils/case1.png'
+import case2 from '../../utils/Workshops.jpg'
 
-import comp from  "../../utils/competetions.jpg"
+import comp from '../../utils/competetions.jpg'
 
-import lec from "../../utils/guestlecture.png"
+import lec from '../../utils/guestlecture.png'
 
-import panal from "../../utils/paneldiscussions.jpeg"
+import panal from '../../utils/paneldiscussions.jpeg'
 
-import nodia from "../../utils/noida.jpg"
-
-
-
+import nodia from '../../utils/noida.jpg'
 
 export default class ComingSoon extends Component {
+  jum = () => {
+    jump('.ignite')
+  }
 
-     jum =()=> {
-         
-        jump(".ignite");
-    }
-    
-    render () {
-        return (
+  render () {
+    return (
+      <div>
+        <Header />
+        <div className='ignite_main'>
 
-            
+          <div className='ignite_text'>
+
+            <div> STARTUP IGNITE</div>
+
             <div>
-                <Header />
-            <div className  = "ignite_main">
+                            Startup Ignite aims to spread awareness about entrepreneurship and startups in various cities PAN-India. Startup Ignite presents students across multiple cities the chance to widen their scope of knowledge in entrepreneurship and meet some of the best people in the field today. E-Cell IITR organises various events in collaboration with local entrepreneurship development
+                            organisations.
 
-                <div className = "ignite_text">
-
-                    <div> STARTUP IGNITE</div>
-
-                    <div>The CAP( Campus Ambassador Programme) is a pinion initiative of E-Summit IIT Roorkee, 2018 organized by the Entrepreneurship Cell, IIT Roorkee and aims to amplify our purview to newer horizons. We aspire to increase our outreach to as many students as possible throughout the country and spread the spirit of entrepreneurship to similar extents. This year E-Summit IIT Roorkee brings with it loads and loads of colossal opportunities for the participating students and exciting perks for the Campus Ambassadors. </div>
-
-                    <div> <img  onClick = {this.jum}  src = {scroll} alt =""></img></div>
-                    
-                    <div> SCROLL DOWN</div>
-                
-                </div>
+                            
             </div>
 
-                <div className = "ignite"> 
-                    
+            <div> <img onClick={this.jum} src={scroll} alt='' /></div>
 
-                    <div className = "ignite_events">
+            <div> SCROLL DOWN</div>
 
-                    <center>
+          </div>
+        </div>
 
-                    <div className = "ignite_head">EVENTS</div>
+        <div className='ignite'>
 
-                        <span className = "ignite_event_unit">
-                            
-                            <div>
-                                <img src = {case1} alt = "case study"></img>
-                            </div>
+          <div className='ignite_events'>
 
-                            <span>
-                            Case Study
-                            </span>
+            <center>
 
-                        </span>
+              <div className='ignite_head'>EVENTS</div>
 
+              <span className='ignite_event_unit'>
 
-                         <span className = "ignite_event_unit">
-                            
-                            <div>
-                            <img src = {case2} alt = "case study"></img>
-                            </div>
-
-                            <span>
-                            Workshop
-                            </span>
-
-                        </span>
-
-                          <span className = "ignite_event_unit">
-                            
-                            <div>
-                            <img src = {comp} alt = "Competitiom "></img>
-                            </div>
-
-                            <span>
-                            Competetions
-                            </span>
-
-                        </span>
-
-
-                          <span className = "ignite_event_unit">
-                            
-                            <div>
-                            <img src = {lec} alt = "Guest Leccture "></img>
-                            </div>
-
-                            <span>
-                            Guest Lecture
-                            </span>
-
-                        </span>
-
-                          <span className = "ignite_event_unit">
-                            
-                            <div>
-                            <img src = {panal} alt = "Panal Discussion "></img>
-                            </div>
-
-                            <span>
-                            Panel Discussion
-                            </span>
-
-                        </span>
-
-             
-                        
-             </center>
-                    
-                    </div>
-
-
-                    <div className = "ignite_head">VENUES</div>
-
-                    <div className = "ignite_venus">
-
-
-                        <center>
-                                <div className = "ignite_venu_unit">
-                                    
-                                    <div>
-                                            <img alt = "jaipur" src = {jaipur} />
-                                    </div>
-
-                                    <span>
-                                        <p>Jaipur </p>
-                                        <p>College: TBD</p>
-                                        <p>Date: TBD</p>                                        
-                                    </span>
-
-                                </div>   
-
-                                <div className = "ignite_venu_unit">
-                                    
-                                    <div>
-                                    <img alt = "Chandigarh" src = {chandi}/>
-                                    </div>
-
-                                    <span>
-                                        <p>Chandigarh </p>
-                                        <p>College: TBD</p>
-                                        <p>Date: TBD</p>                                        
-                                    </span>
-
-                                </div> 
-
-
-                                <div className = "ignite_venu_unit">
-                                    
-                                    <div>
-                                    <img alt = "Dehradun" src = {deh}/>
-
-                                    </div>
-
-                                    <span>
-                                        <p>Dehradun </p>
-                                        <p>College: TBD</p>
-                                        <p>Date: TBD</p>                                        
-                                    </span>
-
-                                </div> 
-
-                                <div className = "ignite_venu_unit">
-                                    
-                                    <div>
-                                    <img alt = "Gurugran " src = {guru}/>
-
-                                    </div>
-
-                                    <span>
-                                        <p>Gurugram </p>
-                                        <p>College: TBD</p>
-                                        <p>Date: TBD</p>                                        
-                                    </span>
-
-                                </div> 
-
-
-                                <div className = "ignite_venu_unit">
-                                    
-                                    <div>
-                                    <img alt = "Noida " src = {nodia}/>
-
-                                    </div>
-
-                                    <span>
-                                        <p>Noida  </p>
-                                        <p>College: TBD</p>
-                                        <p>Date: TBD</p>                                        
-                                    </span>
-
-                                </div>  
-                        </center>
-                    
-                    </div>
+                <div>
+                  <img src={case1} alt='case study' />
                 </div>
- 
 
-                <Footer />            
-            </div>
+                <span>
+                                    Case Study
+                                </span>
 
+              </span>
 
-        );
-    }
+              <span className='ignite_event_unit'>
+
+                <div>
+                  <img src={case2} alt='case study' />
+                </div>
+
+                <span>
+                                    Workshop
+                                </span>
+
+              </span>
+
+              <span className='ignite_event_unit'>
+
+                <div>
+                  <img src={comp} alt='Competitiom ' />
+                </div>
+
+                <span>
+                                    Competetions
+                                </span>
+
+              </span>
+
+              <span className='ignite_event_unit'>
+
+                <div>
+                  <img src={lec} alt='Guest Leccture ' />
+                </div>
+
+                <span>
+                                    Guest Lecture
+                                </span>
+
+              </span>
+
+              <span className='ignite_event_unit'>
+
+                <div>
+                  <img src={panal} alt='Panal Discussion ' />
+                </div>
+
+                <span>
+                                    Panel Discussion
+                                </span>
+
+              </span>
+
+            </center>
+
+          </div>
+
+          <div className='ignite_head'>VENUES</div>
+
+          <div className='ignite_venus'>
+
+            <center>
+              <div className='ignite_venu_unit'>
+
+                <div>
+                  <img alt='jaipur' src={jaipur} />
+                </div>
+
+                <span>
+                  <p>Jaipur </p>
+                  <p>College: TBD</p>
+                  <p>Date: TBD</p>
+                </span>
+
+              </div>
+
+              <div className='ignite_venu_unit'>
+
+                <div>
+                  <img alt='Chandigarh' src={chandi} />
+                </div>
+
+                <span>
+                  <p>Chandigarh </p>
+                  <p>College: TBD</p>
+                  <p>Date: TBD</p>
+                </span>
+
+              </div>
+
+              <div className='ignite_venu_unit'>
+
+                <div>
+                  <img alt='Dehradun' src={deh} />
+
+                </div>
+
+                <span>
+                  <p>Dehradun </p>
+                  <p>College: TBD</p>
+                  <p>Date: TBD</p>
+                </span>
+
+              </div>
+
+              <div className='ignite_venu_unit'>
+
+                <div>
+                  <img alt='Gurugran ' src={guru} />
+
+                </div>
+
+                <span>
+                  <p>Gurugram </p>
+                  <p>College: TBD</p>
+                  <p>Date: TBD</p>
+                </span>
+
+              </div>
+
+              <div className='ignite_venu_unit'>
+
+                <div>
+                  <img alt='Noida ' src={nodia} />
+
+                </div>
+
+                <span>
+                  <p>Noida </p>
+                  <p>College: TBD</p>
+                  <p>Date: TBD</p>
+                </span>
+
+              </div>
+            </center>
+
+          </div>
+        </div>
+
+        <Footer />
+      </div>
+    )
+  }
 }
