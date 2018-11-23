98f1287276881311af1d8c0a9c183d591cc528d2
Tushar Varshney <juniorvarshney19@gmail.com>
2018-11-23 23:40:12 +0530

    ignite campus ambassador home

--- src/components/js/TaskIndex.jsx
+++ src/components/js/TaskIndex.jsx
@@ -1,24 +1,28 @@
-import React,{Component} from 'react'
+import React, { Component } from 'react'
 import Task from './task'
 import './../css/TaskIndex.css'
 
-export default class IndexNew extends Component{
-    constructor(){
+export default class IndexNew extends Component {
+    constructor() {
         super();
-        this.state={
-            tasks:[
-        ]
-        }
-        
-    
-    }
-    render(){
-        return(
-            <div id="tasks">
-            {this.state.tasks ? this.state.tasks.map((tasks,number)=>{
-                    return <Task key={tasks.number} tasks={tasks} number={tasks.number}></Task>//have to wite the key
-                }):null
+        this.state = {
+            tasks: [{
+                description: "bsjbdjabdjabsdljab"
+
             }
+
+            ]
+        }
+
+
+    }
+    render() {
+        return (
+            <div id="tasks">
+                {this.state.tasks ? this.state.tasks.map((tasks, number) => {
+                    return <Task key={tasks.number} tasks={tasks} number={tasks.number}></Task>//have to wite the key
+                }) : null
+                }
             </div>
         )
     }
