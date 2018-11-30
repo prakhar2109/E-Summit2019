356c00cc0b801c26387afc3c65108e7251a78924
prakhar2109 <prakharagarwal21091998@gmail.com>
2018-11-30 08:30:03 +0530

    landing pagefooter modified

--- src/screens/footer/landingpagefooter.jsx
+++ src/screens/footer/landingpagefooter.jsx
@@ -18,31 +18,31 @@ export default class Lfooter extends Component {
                         </div>
 
                         <div className="landing-footer-icons">
-                        
+
                         </div>
 
                         <div className="landing-footer-heading">
-                        Phone no.
+                            Phone no.
                         </div>
 
-                         <div className="landing-footer-details">
-                        7777777777777
+                        <div className="landing-footer-details">
+                            7777777777777
                         </div>
 
                         <div className="landing-footer-heading">
-                        E-Mail ID
+                            E-Mail ID
                         </div>
 
-                         <div className="landing-footer-details">
-                        esummit18@gmail.com
+                        <div className="landing-footer-details">
+                            esummit18@gmail.com
                         </div>
 
-                         <div className="landing-footer-heading">
-                        Our Address
+                        <div className="landing-footer-heading">
+                            Our Address
                         </div>
 
-                         <div className="landing-footer-details">
-                         E-Cell Office, SAC Building, IIT Roorkee, Roorkee, Uttarakhand, India 247667
+                        <div className="landing-footer-details">
+                            E-Cell Office, SAC Building, IIT Roorkee, Roorkee, Uttarakhand,<br/>3.37vh India 247667
                         </div>
                     </div>
                     <div className="footerform">
@@ -50,8 +50,35 @@ export default class Lfooter extends Component {
                         <div className="footerform-heading">
                             GET IN TOUCH
                         </div>
+                        <div className="footerformfiels">
 
+                            <form>
+                            <input type="text" id="formName" placeholder="Name"></input>
+                            <input type="text" id="formPhone"placeholder="Phone no."></input>
+                            <input type="text" id="formEmail" placeholder="E-Mail ID"></input>
+                            <textarea rows="8" cols="50" name="comment" form="usrform" id="formmessage" placeholder="Message">
+                            </textarea>
+
+                            </form>
+
+                        </div>
                     </div>
+
+
+                </div>
+
+                <div className="landing-pagefooterbottom">
+
+                <div className="landing-bottomfooter-heading">
+                For Sporsorhip Opportunities or Collaborations
+                </div>
+
+                 <div className="landing-bottomfooter-mail">
+                 esummit18@gmail.com 
+                </div>
+
+
+
 
 
                 </div>
