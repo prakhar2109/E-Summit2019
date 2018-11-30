f503ca4947efbe78a9ad18e473b24512c912770f
prakhar2109 <prakharagarwal21091998@gmail.com>
2018-11-30 07:20:55 +0530

    landing page updated

--- src/screens/footer/landingpagefooter.jsx
+++ src/screens/footer/landingpagefooter.jsx
@@ -7,31 +7,58 @@ export default class Lfooter extends Component {
 
         return (
 
-            // <div className="landingpagefooter">
-            //     <div className="footerparent">
+            <div className="landingpagefooter">
+
+                <div className="footerparent">
+
+                    <div className="footercontact">
+                        <div className="footer-logo">
 
 
-            //         <div className="footerform">
+                        </div>
 
-            //             <div className="footerform-heading">
-            //             GET IN TOUCH
-            //             </div>
+                        <div className="landing-footer-icons">
+                        
+                        </div>
 
-            //         </div>
-            //         <div className="footercontact">
+                        <div className="landing-footer-heading">
+                        Phone no.
+                        </div>
 
-            //         </div>
+                         <div className="landing-footer-details">
+                        7777777777777
+                        </div>
 
-            //     </div>
+                        <div className="landing-footer-heading">
+                        E-Mail ID
+                        </div>
 
-            // </div>
+                         <div className="landing-footer-details">
+                        esummit18@gmail.com
+                        </div>
+
+                         <div className="landing-footer-heading">
+                        Our Address
+                        </div>
+
+                         <div className="landing-footer-details">
+                         E-Cell Office, SAC Building, IIT Roorkee, Roorkee, Uttarakhand, India 247667
+                        </div>
+                    </div>
+                    <div className="footerform">
+
+                        <div className="footerform-heading">
+                            GET IN TOUCH
+                        </div>
+
+                    </div>
 
 
+                </div>
 
-            <div>
-
-                hello
             </div>
+
+
 
 
         )
