fdb465d28fa4261612c0699978a689c86d9bce27
Rohit <rohitjha941@hotmail.com>
2018-12-11 23:57:43 +0530

    changed svg of faq

--- src/components/singleFaq.jsx
+++ src/components/singleFaq.jsx
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
-import "./css/faq_unit.scss"
+import "./css/faq_unit.scss";
+import Plus from "./static/plus.svg";
+import  cross from "./static/cross.svg";
 class SingleFAQ extends Component {
     constructor(props) {
         super(props);
@@ -15,15 +17,19 @@ class SingleFAQ extends Component {
                 .display = 'block';
 
             var qid = 'q' + id;
-            var iid = 'i' + id;
+            var pid = 'p' + id;
+            var cid = 'c' + id;
             document
                 .getElementById(qid)
                 .style
                 .background = '#F7F8F9';
             document
-                .getElementById(iid)
-                .classList
-                .add('fa-times-circle');
+                .getElementById(pid)
+                .style.display ="none";
+
+                document
+                .getElementById(cid)
+                .style.display ="inline-block";
 
             this.setState({show: 0});
         } else {
@@ -32,15 +38,22 @@ class SingleFAQ extends Component {
                 .style
                 .display = 'none';
             qid = 'q' + id;
-            iid = 'i' + id;
+            cid = 'c' + id;
+            pid = 'p' + id;
             document
                 .getElementById(qid)
                 .style
                 .background = 'white';
+    
+
             document
-                .getElementById(iid)
-                .classList
-                .remove('fa-times-circle');
+                .getElementById(pid)
+                .style.display ="inline-block";
+
+             document
+                .getElementById(cid)
+                .style.display ="none";
+
             this.setState({show: 1});
         }
     };
@@ -53,7 +66,8 @@ class SingleFAQ extends Component {
                 className="faq_unit">
                 <div className="question" id={'q' + update.id}>
                     {update.question}
-                    <i id={'i' + update.id} className="fas fa-plus-circle"></i>
+                    <img className = "plus" src = {Plus}  id={'p' + update.id} /> 
+                    <img className = "cross" src = {cross}  id={'c' + update.id} /> 
                 </div>
                 <div className="answer" id={update.id}>
                     {update.answer}
