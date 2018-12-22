88e662cb69c7f7aec0ebd4ff94ea7d946cdacca2
Rohit <rohitjha941@hotmail.com>
2018-12-22 22:42:36 +0530

    fixed in landing page

--- src/NewScreens/landing/about.js
+++ src/NewScreens/landing/about.js
@@ -7,14 +7,14 @@ class About extends Component {
             <section id="about">
             <div className="land_screen2">
                 <Header title="WHAT IS E-SUMMIT?"/>
-                <p>eSummit being the flagship event of ECell, is held annually brings together
+                <p className = "p">eSummit being the flagship event of ECell, is held annually brings together
                     the academic community, venture capitalists, new age entrepreneurs and all those
                     passionate about entrepreneurship to common grounds. It provides an avenue to
                     exhibit the entrepreneurial talent and creativity through many events like
                     business ventures, product design competition, and social entrepreneurial idea
                     competition.</p>
                 <Header title="E-SUMMIT’19  BUILDING FOR BILLION"/>    
-                <p>Several business ventures work out and end up as successes and unfortunately,
+                <p className = "p">Several business ventures work out and end up as successes and unfortunately,
                     a lot more of them don’t. But all of them share a common denominator, they
                     started out as an IDEA. It doesn’t matter where you get it, under the shower, on
                     an evening walk or at the coffee table. Some are good, some bad, and some might
