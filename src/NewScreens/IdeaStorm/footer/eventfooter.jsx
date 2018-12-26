38bda7cc1eb05b555104ef1c785c369a7e4bd684
ritiksp <ritikiitrbiotech@gmail.com>
2018-12-26 21:28:21 +0530

    linked in link change

--- src/NewScreens/IdeaStorm/footer/eventfooter.jsx
+++ src/NewScreens/IdeaStorm/footer/eventfooter.jsx
@@ -101,7 +101,7 @@ export default class Lfooter extends Component {
                                 <a rel="noopener noreferrer" href="https://www.instagram.com/ecelliitr/" target="_blank"><i class="fab fa-instagram"></i></a>
                                 <a rel="noopener noreferrer" href="https://www.youtube.com/channel/UCf1py66Ov1easJAswwjxQZw" target="_blank"><i class="fab fa-youtube" target="_blank"></i></a>
                                 <a rel="noopener noreferrer" href="https://twitter.com/EDC_IITRoorkee" target="_blank"><i class="fab fa-twitter"></i></a>
-                                <a rel="noopener noreferrer" href="https://www.linkedin.com/company/esummit/" target="_blank"><i class="fab fa-linkedin"></i></a>
+                                <a rel="noopener noreferrer" href="https://www.linkedin.com/company/ecelliitr/" target="_blank"><i class="fab fa-linkedin"></i></a>
 
 
                             </div>
