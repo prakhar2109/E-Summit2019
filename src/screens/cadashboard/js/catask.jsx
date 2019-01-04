860a0cc0ba21ecbc1d2ca734c2f3e09f09fe38ed
prakhar2109 <prakharagarwal21091998@gmail.com>
2019-01-04 05:46:25 +0530

    new taskv02

--- src/screens/cadashboard/js/catask.jsx
+++ src/screens/cadashboard/js/catask.jsx
@@ -1,7 +1,7 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import '../css/catask.css';
 import axios from "axios";
-import {BASE_URL} from './../../../utils/urls';
+import { BASE_URL } from './../../../utils/urls';
 let token = localStorage.getItem('user_token')
 export default class CATaskBoard extends Component {
 
@@ -16,14 +16,14 @@ export default class CATaskBoard extends Component {
 
         axios
             .get(BASE_URL + '/v1/api/task/list/', {
-            'headers': {
-                'Authorization': `Token ${token}`
-            }
-        })
+                'headers': {
+                    'Authorization': `Token ${token}`
+                }
+            })
             .then(res => {
                 if (res && res.data) {
                     console.log(res.data, "res.data")
-                    this.setState({tasks: res.data})
+                    this.setState({ tasks: res.data })
                 }
             })
             .catch(function (response) {
@@ -33,11 +33,11 @@ export default class CATaskBoard extends Component {
     }
 
     render() {
-        let {tasks} = this.state;
+        let { tasks } = this.state;
         let today = new Date();
         let dayToday = today.getDate();
         console.log("dayToday", dayToday)
-        let dateString = (dayToday <= 31 && dayToday > 26) ? '24 Dec - 31 Dec 2018' :  (dayToday < 8  ? '1 Jan - 7 Jan 2019' :  (dayToday < 15  ? '8 Jan - 14 Jan 2019' :  (dayToday < 22  ? '15 Jan - 21 Jan 2019' :  (dayToday < 29  ? '22 Jan - 28 Jan 2019' : '29 Jan - 2 Feb 2019'))));
+        let dateString = (dayToday <= 31 && dayToday > 26) ? '24 Dec - 31 Dec 2018' : (dayToday < 8 ? '1 Jan - 7 Jan 2019' : (dayToday < 15 ? '8 Jan - 14 Jan 2019' : (dayToday < 22 ? '15 Jan - 21 Jan 2019' : (dayToday < 29 ? '22 Jan - 28 Jan 2019' : '29 Jan - 2 Feb 2019'))));
         return (
 
             <div className="taskparent">
@@ -52,7 +52,7 @@ export default class CATaskBoard extends Component {
                 </div>
                 <div className="taskchildrow">
 
-                    {tasks && tasks.map((e) => <CATask key={e.id} task={e}/>)}
+                    {tasks && tasks.map((e) => <CATask key={e.id} task={e} />)}
 
                 </div>
             </div>
@@ -67,7 +67,7 @@ class CATask extends Component {
             isfileUploaded: false,
             fileUploaded: null
         }
-    } 
+    }
     state = {
         visible: true
     }
@@ -81,17 +81,17 @@ class CATask extends Component {
             this.setState({
                 fileUploaded,
                 isfileUploaded
-            }, () => {})
+            }, () => { })
             let formData = new FormData();
             formData.append("file", file[0]);
             formData.append("task", task.id);
             axios
                 .post(BASE_URL + `/v1/api/task/${task.id}/submit/`, formData, {
-                headers: {
-                    'Authorization': `Token ${token}`,
-                    'Content-Type': 'multipart/formdata '
-                }
-            })
+                    headers: {
+                        'Authorization': `Token ${token}`,
+                        'Content-Type': 'multipart/formdata '
+                    }
+                })
                 .then(() => {
 
                     // alert(resData) //do something with this and show the user that the file has
@@ -99,9 +99,9 @@ class CATask extends Component {
                     document
                         .getElementById(`nameOfFileUploadedForTask${task.id}`)
                         .innerHTML = name
-                        .files
-                        .item(0)
-                        .name;
+                            .files
+                            .item(0)
+                            .name;
                     document
                         .getElementById(`filestatus${task.id}`)
                         .innerHTML = "File Uploaded ";
@@ -124,20 +124,30 @@ class CATask extends Component {
                         {task.description}
 
                     </div>
+                    {(task.sub === null || task.sub === undefined) ?
+                    <div className="taskchild-uploadedfiles">
+                   
+                       uploaded files shown here
+                   
+                    </div>
+                    : 
+                    <div className="taskchild-filesupload">
+                        <div className="Selectfilesvg">
+                        <p id={`nameOfFileUploadedForTask${task.id}`} className="taskName"></p>
 
+                        <p id={`filestatus${task.id}`} className="taskName">File uploaded</p>
+                        </div>
+                    </div>
+                    }
                     <div className="taskchild-fileupload">
 
                         <input
                             id={`fileInput${task.id}`}
                             type='file'
                             className="filesvg"
-                            onChange={(e) => this.fileUploadHandler(e.target.files, task)}/>
-                        <div className="Selectfilesvg">
-                            <p id={`nameOfFileUploadedForTask${task.id}`} className="taskName"></p>
-
-                            <p id={`filestatus${task.id}`} className="taskName">{(task.sub === null || task.sub === undefined) ? 'Choose a File': 'File Uploaded'}</p>
-                        </div>
-
+                            onChange={(e) => this.fileUploadHandler(e.target.files, task)} />
+                           
+                        
                     </div>
                 </div>
             </div>
