8e35f375a6ecaa0c74f8c6a4790f2eab7bca4819
Ritik Singh Panwar <33784820+ritiksp@users.noreply.github.com>
2019-01-04 13:21:40 +0530

    content update

--- src/components/Productathon/Index.jsx
+++ src/components/Productathon/Index.jsx
@@ -237,13 +237,7 @@ Team Size: 3-5.
                             There is no restriction on any individual to participate. Members of all educational and professional fraternities are welcome to participate.
                         </li>
 
-                        <li>
-                            Start-ups which have been incorporated more than a year ago (i.e. registered on or before 1 December, 2017) are not eligible to participate in the Idea Storm.
-                        </li>
-                        <li>
-                            Ventures that have acquired funding from external investor-networks (angels, VC's, etc) can not participate. We believe in promoting startups that require assistance in the form of seed capital, mentoring and various types of consultancies.
-                        </li>
-                    </ol>
+                     </ol>
                 </div>
 
                 <div className="erks">
