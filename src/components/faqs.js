1868c9817b1bc5e9adec6af93f16abd8adb5b02e
Pulkit Gupta <pulkonet@gmail.com>
2019-01-06 19:33:29 +0530

    fixes most of lint errors

--- src/components/faqs.js
+++ src/components/faqs.js
@@ -1,53 +1,49 @@
 import React, { Component } from "react";
+import { Element } from "react-scroll";
+import { NavLink } from "react-router-dom";
 import SingleFAQ from "./singleFaq";
 import Header from "./header";
-import {Element} from "react-scroll";
-import {NavLink } from "react-router-dom";
-class FAQBoard extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      show: 1,
-    };
-  }
-  toggle = (e, id) => {
-    if (this.state.show === 1) {
-      document.getElementById(id).style.display = "block";
 
-      var qid = "q" + id;
-      var iid = "i" + id;
+class FAQBoard extends Component {
+  state = {
+    show: 1,
+  };
+
+  toggle = (e, id) => {
+    const qid = `q${id}`;
+    const iid = `i${id}`;
+    const { show } = this.state;
+    if (show === 1) {
+      document.getElementById(id).style.display = "block";
       document.getElementById(qid).style.background = "#F7F8F9";
       document.getElementById(iid).classList.add("fa-times-circle");
 
       this.setState({ show: 0 });
     } else {
       document.getElementById(id).style.display = "none";
-      qid = "q" + id;
-      iid = "i" + id;
       document.getElementById(qid).style.background = "white";
       document.getElementById(iid).classList.remove("fa-times-circle");
       this.setState({ show: 1 });
     }
   };
-  render() {
 
-    
+  render() {
+    const { faqList } = this.props;
     return (
-      <Element id = "faq" name ="faq">
+      <Element id="faq" name="faq">
         <section id="faq">
           <div className="land_faq">
             <Header title="FAQs" />
             <p>Stuck somewhere ? We’ re here to help you! </p>
-        
 
-            {this.props.faqList.map(update => {
-              
-                  return <SingleFAQ key = {update.id}update={update} />;
-                  
-                })}
+            {faqList.map(update => (
+              <SingleFAQ key={update.id} update={update} />
+            ))}
 
             <center>
-              <NavLink to = "/faq" > <button className="view_all"> VIEW ALL </button></NavLink>
+              <NavLink to="/faq">
+                <button className="view_all"> VIEW ALL </button>
+              </NavLink>
             </center>
           </div>
         </section>
