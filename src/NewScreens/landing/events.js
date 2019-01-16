f450c64025b75f8772c37941695f5748cb5533e3
Pulkit Gupta <pulkonet@gmail.com>
2019-01-16 10:48:08 +0530

    landing page events live

--- src/NewScreens/landing/events.js
+++ src/NewScreens/landing/events.js
@@ -1,21 +1,20 @@
 import React, { Component } from "react";
 import Header from "../../components/header";
 import { Element } from "react-scroll";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import ViewMore from "./static/view_more.png";
 // import { NavLink } from "react-router-dom"
 class Events extends Component {
   constructor() {
     super();
     this.state = {
-      data: ''
-    }
+      data: "",
+    };
   }
   render() {
-    let { data } = this.state
+    let { data } = this.state;
     return (
       <>
-
         <Element id="events" name="events">
           <div className="land_events">
             <Header title="HIGHLIGHT EVENTS" />
@@ -28,10 +27,10 @@ class Events extends Component {
           <div className="land_events_array">
             <div>
               <a href="/ideastorm" target="_blank">
-                <div className="conclave" >
+                <div className="conclave">
                   <span>
                     <p className="title">IDEASTORM</p>
-                    <p className="tagline"></p>
+                    <p className="tagline" />
                   </span>
                   <span>
                     <center>
@@ -44,25 +43,29 @@ class Events extends Component {
                 </div>
               </a>
             </div>
-            <div className="workshop">
-              <span>
-                <p className="title">CONCLAVES</p>
-                <p className="tagline"></p>
-              </span>
-              <span>
-                <center>
-                  {/* <i>
-                  
-                    <img src={ViewMore} alt="view more" />
-                  </i> */}
-                  <p className="viewmore">COMING SOON</p>
-                </center>
-              </span>
+            <div>
+              <a href="/events/conclaves" target="_blank">
+                <div className="workshop">
+                  <span>
+                    <p className="title">CONCLAVES</p>
+                    <p className="tagline" />
+                  </span>
+                  <span>
+                    <center>
+                      <i>
+                        <img src={ViewMore} alt="view more" />
+                      </i>
+                      <p className="viewmore">VIEW MORE</p>
+                    </center>
+                  </span>
+                </div>
+              </a>
             </div>
+
             <div className="leadership">
               <span>
                 <p className="title">LEADERSHIP SERIES</p>
-                <p className="tagline"></p>
+                <p className="tagline" />
               </span>
               <span>
                 <center>
@@ -77,7 +80,7 @@ class Events extends Component {
             <div className="competition">
               <span>
                 <p className="title">WORKSHOPS</p>
-                <p className="tagline"></p>
+                <p className="tagline" />
               </span>
 
               <span>
@@ -95,7 +98,9 @@ class Events extends Component {
             <Header title="EVENTS " />
 
             <p className="text">
-              B-Plan, Case Studies, Productathon, Networking Events and a lot more to explore! Register for the events and compete among the best minds of the country.
+              B-Plan, Case Studies, Productathon, Networking Events and a lot
+              more to explore! Register for the events and compete among the
+              best minds of the country.
             </p>
             <center>
               <div className="land_events_array2">
@@ -103,17 +108,15 @@ class Events extends Component {
                   <div className="events_production" id="events_production1">
                     <span>
                       <p className="t1">PRODUCTATHON</p>
-
                     </span>
                     <span>
                       <center>
-
                         <i>
-
                           <img src={ViewMore} alt="view more" />
                         </i>
-                        <p className="viewmore esummit-landing-view-more-button">VIEW MORE</p>
-
+                        <p className="viewmore esummit-landing-view-more-button">
+                          VIEW MORE
+                        </p>
                       </center>
                     </span>
                   </div>
@@ -122,28 +125,23 @@ class Events extends Component {
                   <div className="events_production" id="events_production7">
                     <span>
                       <p className="t1">E-CONFERENCE</p>
-
                     </span>
                     <span>
                       <center>
-
                         <i>
-
                           <img src={ViewMore} alt="view more" />
                         </i>
-                        <p className="viewmore esummit-landing-view-more-button">VIEW MORE</p>
-
+                        <p className="viewmore esummit-landing-view-more-button">
+                          VIEW MORE
+                        </p>
                       </center>
                     </span>
                   </div>
                 </Link>
 
-
-
                 <div className="events_production" id="events_production3">
                   <span>
                     <p className="t1">STARTUP EXPO</p>
-
                   </span>
                   <p className="t3">COMING SOON</p>
                 </div>
@@ -151,23 +149,36 @@ class Events extends Component {
                 <div className="events_production" id="events_production4">
                   <span>
                     <p className="t1">CASE STUDY CHALLENGE</p>
-
                   </span>
                   <p className="t3">COMING SOON</p>
                 </div>
 
-                <div className="events_production" id="events_production5">
+                {/* <div className="events_production" id="events_production5">
                   <span>
                     <p className="t1">ONLINE EVENTS</p>
-
                   </span>
                   <p className="t3">COMING SOON</p>
-                </div>
-
+                </div> */}
+                <Link to="/events/onlineevents">
+                  <div className="events_production" id="events_production5">
+                    <span>
+                      <p className="t1">ONLINE EVENTS</p>
+                    </span>
+                    <span>
+                      <center>
+                        <i>
+                          <img src={ViewMore} alt="view more" />
+                        </i>
+                        <p className="viewmore esummit-landing-view-more-button">
+                          VIEW MORE
+                        </p>
+                      </center>
+                    </span>
+                  </div>
+                </Link>
                 <div className="events_production" id="events_production6">
                   <span>
                     <p className="t1">CORPORATE SPEED DATING</p>
-
                   </span>
                   <p className="t3">COMING SOON</p>
                 </div>
